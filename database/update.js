2502af2faa48d4e132d26cacd6df0da290500368
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-07-21 00:35:16 +0400

    fuck microsoft

--- database/update.js
+++ database/update.js
@@ -1,17 +1,18 @@
+import Note from "../models/noteModel.js";
 
 async function updateDocuments() {
-    // try {
-    //     const documents = await Note.find({});
-    //     for (let doc of documents) {
-    //         if (!doc.likes) {
-    //             doc.likes = new Array();
-    //         };
-    //         await doc.save();
-    //     };
-    //     console.log('All documentation updated');
-    // } catch (err) {
-    //     console.log(err);
-    // };
+    try {
+        const documents = await Note.find({});
+        for (let doc of documents) {
+            if (!doc.likes) {
+                doc.likes = new Array();
+            };
+            await doc.save();
+        };
+        console.log('All documentation updated');
+    } catch (err) {
+        console.log(err);
+    };
 };
 
 export default updateDocuments;
