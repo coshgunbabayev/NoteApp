1bf1e73424d668c9fa7076f2f074312774d543fa
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-07-27 03:37:21 +0400

    u1

--- database/update.js
+++ database/update.js
@@ -1,16 +1,28 @@
 import User from "../models/userModel.js";
 
 async function updateDocuments() {
-    // try {
-    //     const documents = await User.find({});
-    //     for (let doc of documents) {
-    //         delete doc.links
-    //         await doc.save();
-    //     };
-    //     console.log('All documentation updated');
-    // } catch (err) {
-    //     console.log(err);
-    // };
+    try {
+        const documents = await User.find({});
+        for (let doc of documents) {
+            if(!doc.bio) {
+                doc.bio = '';
+                await doc.save();
+            };
+
+            if (!doc.profilePicture) {
+                doc.profilePicture = '';
+                await doc.save();
+            };
+
+            if (!doc.profilePictureId) {
+                doc.profilePictureId = '';
+                await doc.save();
+            };
+        };
+        console.log('All documentation updated');
+    } catch (err) {
+        console.log(err);
+    };
 };
 
 export default updateDocuments;
